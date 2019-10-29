54b3e922a9b12cab97a59a93044d239fb22eec43
nurdeafni <nurdeafni@gmail.com>
2019-10-28 21:09:11 -0700

    config body parser

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+//memanggil library body parser
+const bodyParser = require('body-parser');
+//config body parser
+app.use(bodyParser.urlencoded({extended: true})); // menangkap type request dalam bentuk from urlencoded
+app.use(bodyParser.json()); // menangkap url dalam bentuk json
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!')) 
 // run aplikasi
 
 //membuat request post

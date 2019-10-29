5b564be19e41b91aebe84515484701304f4afb9a
nurdeafni <nurdeafni@gmail.com>
2019-10-28 20:55:09 -0700

    membuat request post

--- server.js
+++ server.js
@@ -2,39 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-const mongoose= require('mongoose');
-
-mongoose.connect('mongodb://localhost/belajarmongo');
-//bikin model untuk Collection person
-const PersonModel = mongoose.model('person',{
-    firstName: String,
-    lastName: String
-});
-
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/hello',function(req,res){
+// run aplikasi
+
+//membuat request post
+app.post('/helo',function (req,res){
     const respon ={
         statusCode : 200,
         error :'',
-        message : 'hello json'
+        message: 'hello json',
     }
     res.json(respon);
 })
 
-app.post('/create',async (req,res) => {
-    console.log(req.body);
-    var person= new PersonModel(req.body);
-    var result= await person.save();
-    const response ={
-        statusCode : 200,
-        error :'',
-        message: 'create person',
-        content : result
-    }
-    res.json(response);
-})
+// commit lagi dengan nama "membuat request post"
 app.listen(port, () => console.log (`Example app Listening on port ${port}!` ))
